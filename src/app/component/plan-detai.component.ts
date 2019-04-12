d8551ed7a53145c99d9d559f6f976a6c21ed9d64
almiraj <rain@almiraj.net>
2019-04-12 18:17:11 +0900

    動かない

--- src/app/component/plan-detai.component.ts
+++ src/app/component/plan-detai.component.ts
@@ -204,6 +204,30 @@ export class PlanDetailComponent {
     });
   }
   share() {
-    
+    try {
+// this is the complete list of currently supported params you can pass to the plugin (all optional)
+var options = {
+  message: 'share this', // not supported on some apps (Facebook, Instagram)
+  subject: 'the subject', // fi. for email
+  files: [], // an array of filenames either locally or remotely
+  url: '',
+  chooserTitle: 'Pick an app', // Android only, you can override the default share sheet title,
+  appPackageName: '' // Android only, you can provide id of the App you want to share with
+};
+
+var onSuccess = function(result) {
+  alert("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+  alert("Shared to app: " + result.app); // On Android result.app since plugin version 5.4.0 this is no longer empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+};
+
+var onError = function(msg) {
+  alert("Sharing failed with message: " + msg);
+};
+
+// window['cordova'].plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
+window['plugins'].socialsharing.shareWithOptions(options, onSuccess, onError);
+    } catch (e) {
+      alert(String(e));
+    }
   }
 }
