fea9260e4b83f06ee1cec7bcafb3e3a7d39b6f9e
almiraj <rain@almiraj.net>
2019-05-17 11:34:02 +0900

    Add firebase config to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB5rkqpz-grkDVMJZQJXhlrnwHj-Mb70Lo',
+    authDomain: 'shiori-chan.firebaseapp.com',
+    databaseURL: 'https://shiori-chan.firebaseio.com',
+    projectId: 'shiori-chan',
+    storageBucket: 'shiori-chan.appspot.com',
+    messagingSenderId: '896368365389'
+  }
 };
